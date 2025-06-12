cb52ee34c59b8ac7c5ba194409e8141acda5345d
crljhnmngs <manigoscarljohn@gmail.com>
2025-06-13 01:37:23 +0800

    chore(BE): initial commit

--- server/src/routes/userRouter.ts
+++ server/src/routes/userRouter.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import {
     getUserProfile,
     registerUser,
+    updateUserProfile,
 } from '../controllers/User/userController';
 import upload from '../config/multerConfig';
 import { requireAuth } from '../middlewares/requireAuth';
@@ -10,5 +11,11 @@ const userRouter = Router();
 
 userRouter.post('/', upload.single('profileImage'), registerUser);
 userRouter.get('/profile', requireAuth, getUserProfile);
+userRouter.patch(
+    '/update-profile',
+    upload.single('profileImage'),
+    requireAuth,
+    updateUserProfile
+);
 
 export default userRouter;
---
+++ server/src/utils/helper.ts
@@ -0,0 +1,11 @@
+export const assignFields = <T extends object>(
+    target: T,
+    source: Partial<T>,
+    fields: (keyof T)[]
+): void => {
+    for (const field of fields) {
+        if (field in source && source[field] !== undefined) {
+            target[field] = source[field]!;
+        }
+    }
+};
